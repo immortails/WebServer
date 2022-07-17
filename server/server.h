0000dca133eb15c2616b13b5e8246ec7a3fb1a1f
immortails <z738313279@126.com>
2022-07-16 23:48:46 -0700

    修复误删除server

---
+++ server/server.h
@@ -0,0 +1,51 @@
+#ifndef SERVER_H
+#define SERVER_H 
+
+#include<iostream>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include<stdio.h>
+#include<unistd.h>
+#include<errno.h>
+#include<string.h>
+#include<fcntl.h>
+#include<stdlib.h>
+#include<cassert>
+#include<sys/epoll.h>
+#include<iostream>
+#include<errno.h>
+#include<unordered_set>
+#include "../locker/locker.h"
+#include "../threadPool/threadpool.h"
+#include "../http/httpConn.h"
+#include "../config.h"
+#include "../timer/heapTimer.h"
+
+
+
+class server{
+private:
+    int listenfd;
+    int epollfd;
+    threadpool<clientData>* pool=nullptr;
+    epoll_event* events;
+    clientData* users;
+    //下面都是定时器的操作
+    char signals[1024];
+    char closefdFromHC[1024];
+    bool timeout=false;
+    void static sigHandler(int sig);        //定时器回调函数要设成静态的，不然传不进去参数
+    void static timerHandler();
+public:
+static int pipefd[2];
+    int pipefdHttp[2];
+    server();
+    ~server();
+    void initThreadPool(int threadNumber=g_conf.THREAD_NUMBER,int maxRequests=g_conf.MAX_REQUESTS);          //初始化线程池
+    void initSocket();              //初始化连接
+    void init();                    //初始化
+    void workLoop();                //主循环
+};
+
+#endif
